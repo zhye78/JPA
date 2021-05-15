65e3f8f5774a502a6d40e1c20e8b6dbe4dfa9041
zhye78 <ccoje2155@naver.com>
2021-05-15 13:01:46 +0900

    상속 클래스, 상속관계 매핑 추가

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Item;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -16,6 +18,11 @@ public class JpaMain {
         transaction.begin();
 
         try {
+
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+            entityManager.persist(book);
 
             transaction.commit();
         }catch (Exception e){
--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -5,7 +5,7 @@ import com.sun.org.apache.xpath.internal.operations.Or;
 import javax.persistence.*;
 
 @Entity
-public class Delivery {
+public class Delivery extends BaseEntity{
 
     @Id @GeneratedValue
     @Column(name = "DELIVERY_ID")
